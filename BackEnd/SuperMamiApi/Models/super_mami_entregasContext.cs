0aa2611ae888c83faceb7fd6a1ef56fd686ef12a
unknown <111961@tecnicatura.frc.utn.edu.ar>
2021-10-17 20:32:09 -0300

    usuario controller realizado

--- BackEnd/SuperMamiApi/Models/super_mami_entregasContext.cs
+++ BackEnd/SuperMamiApi/Models/super_mami_entregasContext.cs
@@ -35,7 +35,7 @@ namespace SuperMamiApi.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseNpgsql("User ID=prog3; Password=Admin1234;Server=localhost; Database=super_mami_entregas;Integrated Security=true;Pooling=true");
             }
         }
