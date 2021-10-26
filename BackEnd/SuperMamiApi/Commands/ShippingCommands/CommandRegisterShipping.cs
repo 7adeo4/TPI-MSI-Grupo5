6bc18665a7041da5aee51b34c1428d401e763b7e
unknown <111961@tecnicatura.frc.utn.edu.ar>
2021-10-25 22:41:49 -0300

    Shipping controller

--- BackEnd/SuperMamiApi/Commands/ShippingCommands/CommandRegisterShipping.cs
+++ BackEnd/SuperMamiApi/Commands/ShippingCommands/CommandRegisterShipping.cs
@@ -6,10 +6,8 @@ namespace SuperMamiApi.Commands.ShippingCommands
     {
 
         public int? IdShippingCompany { get; set; }
-        public int? IdState { get; set; }
         public int? IdDeliveryOrder { get; set; }
         public int? IdUser { get; set; }
-        public bool? IsActive { get; set; }
 
     }
 }
