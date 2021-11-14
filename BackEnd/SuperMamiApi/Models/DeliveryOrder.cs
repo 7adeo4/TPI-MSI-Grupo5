aa3cd146839d6ce078b90321dbfafd6b01836419
1w1111995RomeroCarmela <111995@tecnicatura.frc.utn.edu.ar>
2021-11-14 10:33:50 -0300

    Listado pickups

--- BackEnd/SuperMamiApi/Models/DeliveryOrder.cs
+++ BackEnd/SuperMamiApi/Models/DeliveryOrder.cs
@@ -18,13 +18,13 @@ namespace SuperMamiApi.Models
         public string Surname { get; set; }
         public string Phone { get; set; }
         public string Address { get; set; }
-        public DateTime? DeliveryDate { get; set; }
+        public DateTime DeliveryDate { get; set; }
         public int? IdZone { get; set; }
         public int? IdBranch { get; set; }
-        public bool? IsOwner { get; set; }
-        public bool? IsShipping { get; set; }
+        public bool IsOwner { get; set; }
+        public bool IsShipping { get; set; }
         public double? ShippingPrice { get; set; }
-        public bool? IsFree { get; set; }
+        public bool IsFree { get; set; }
 
         public virtual Branch IdBranchNavigation { get; set; }
         public virtual Zone IdZoneNavigation { get; set; }
