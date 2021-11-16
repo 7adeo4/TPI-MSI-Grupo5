db06424d858302d8450c77ce1a465be1331255bd
7adeo4 <7adeo4@gmail.com>
2021-11-16 10:02:05 -0300

    aa

--- FrontEnd/js/getCompleteName.js
+++ FrontEnd/js/getCompleteName.js
@@ -1,8 +1,3 @@
 document.getElementById('userNav').textContent = `Bienvenido ${localStorage.getItem('userPassword')}`
 
 
-if(localStorage.getItem('boss') == 1 || localStorage.getItem('boss') == 2){
-    document.getElementById('adminBack').classList.add('adminBack_active');
-}else{
-    document.getElementById('adminBack').classList.remove('adminBack_active');
-}
